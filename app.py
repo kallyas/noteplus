465db663ed3dfd97bc708b1f5c1686d52bd1858b
kallyas <kallyasmedia@gmail.com>
2022-10-15 16:19:47 +0300

    ch: initialize flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from notesapp.config import app_config
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config.from_object(app_config[os.environ['FLASK_DEBUG']])
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/auth/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ notesapp/config.py
@@ -0,0 +1,47 @@
+# This file contains the configuration for the flask application
+# for various environments
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname('uploads'))
+TEMPLATES_FOLDER = os.path.abspath(os.path.dirname('templates'))
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+STATIC_FOLDER = os.path.abspath(os.path.dirname('static'))
+
+
+# Base configuration
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ['SECRET_KEY']
+    UPLOAD_FOLDER = UPLOAD_FOLDER
+    TEMPLATES_FOLDER = TEMPLATES_FOLDER
+    STATIC_FOLDER = STATIC_FOLDER
+    ALLOWED_EXTENSIONS = ALLOWED_EXTENSIONS
+
+# Development configuration
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+# Testing configuration
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///notesapp.db'
+
+# Production configuration
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL_PRODUCTION']
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
