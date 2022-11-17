b2981374a58ece4ff001eeb849d9087e5ee8f125
Kally <60466044+kallyas@users.noreply.github.com>
2022-11-17 22:45:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Tables created")
-    PORT = int(os.environ.get('PORT', 5000))
+    PORT = int(os.environ.get('PORT', 8000))
     app.run('0.0.0.0', PORT)
